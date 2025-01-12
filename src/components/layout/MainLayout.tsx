4c06921a1744faec20215cb8ce1b91bfa1a3295f
nazim1971 <md.nazimuddinaj@gmail.com>
2025-01-12 22:55:22 +0600

    Work on ant design siderbar

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,44 @@
+import { Layout, Menu } from "antd";
+
+const {Content,Header,Sider,Footer} = Layout
+
+
+const MainLayout = () => {
+    return (
+        <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="demo-logo-vertical" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    );
+};
+
+export default MainLayout;
