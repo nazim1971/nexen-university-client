f97466e8b75ab1d76791be45d1b6bcd7ade413db
nazim1971 <md.nazimuddinaj@gmail.com>
2025-01-14 00:23:57 +0600

    add separeate fill in adminroute

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,90 +1,90 @@
-
 import { Layout, Menu, MenuProps, theme } from "antd";
 import { Suspense } from "react";
 import { NavLink, Outlet } from "react-router";
 
+const { Content, Header, Sider, Footer } = Layout;
 
-const {Content,Header,Sider,Footer} = Layout;
-
-
-const items: MenuProps['items'] = [
-    {
-        key: 'home',
-        label: <NavLink  to='/'> Home </NavLink>
-    },
-    {
-        key: 'dashboard',
-        label: <NavLink  to='/admin/dashboard'> Dashboard </NavLink>
-    },
-    {
-        key: '3',
-        label: 'User management',
-        children:[
-            {
-                key: 'create-student',
-                label: <NavLink  to='/admin/create-admin'> Create-Admin </NavLink>
-            },
-            {
-                key: 'create-student',
-                label: <NavLink  to='/admin/create-student'> Create-Student </NavLink>
-            },
-            {
-              key: 'create-faculty',
-              label: <NavLink  to='/admin/create-faculty'> Create-Faculty </NavLink>
-          }
-        ]
-    }
-]
-
+const items: MenuProps["items"] = [
+  {
+    key: "home",
+    label: <NavLink to="/"> Home </NavLink>,
+  },
+  {
+    key: "dashboard",
+    label: <NavLink to="/admin/dashboard"> Dashboard </NavLink>,
+  },
+  {
+    key: "3",
+    label: "User management",
+    children: [
+      {
+        key: "create-admin",
+        label: <NavLink to="/admin/create-admin"> Create-Admin </NavLink>,
+      },
+      {
+        key: "create-student",
+        label: <NavLink to="/admin/create-student"> Create-Student </NavLink>,
+      },
+      {
+        key: "create-faculty",
+        label: <NavLink to="/admin/create-faculty"> Create-Faculty </NavLink>,
+      },
+    ],
+  },
+];
 
 const MainLayout = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
-    const {
-        token: { colorBgContainer, borderRadiusLG },
-      } = theme.useToken();
-
-    return (
-        <Layout style={{height: "100vh"}} >
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-            <div className=" text-center text-white text-xl font-semibold mt-4">
-                <h1>Nexen University</h1>
-            </div>
-          <div className="demo-logo-vertical" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-        </Sider>
-        <Layout>
-          <Header style={{ padding: 0, background: colorBgContainer }} />
-          <Content style={{ margin: '24px 16px 0' }}>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 360,
-                background: colorBgContainer,
-                borderRadius: borderRadiusLG,
-              }}
-            >
-             <main>
-        <Suspense fallback={<div>Loading page...</div>}>
-          <Outlet /> {/* Nested routes are rendered here */}
-        </Suspense>
-      </main>
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©{new Date().getFullYear()} Created by Ant UED
-          </Footer>
-        </Layout>
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className=" text-center text-white text-xl font-semibold mt-4">
+          <h1>Nexen University</h1>
+        </div>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <main>
+              <Suspense fallback={<div>Loading page...</div>}>
+                <Outlet /> {/* Nested routes are rendered here */}
+              </Suspense>
+            </main>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
       </Layout>
-    );
+    </Layout>
+  );
 };
 
 export default MainLayout;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,59 +1,11 @@
-// import { lazy } from 'react';
-// import { RouteObject } from 'react-router';
-
 import { Route, Routes } from "react-router";
 import MainLayout from "../components/layout/MainLayout";
 import Home from "../pages/Home";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateStudent from "../pages/admin/CreateStudent";
 import About from "../pages/About";
-import CreateFaculty from "../pages/admin/CreateFaculty";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
-import CreateAdmin from "../pages/admin/CreateAdmin";
+import AdminRoutes from "./Admin.routes";
 
-// const MainLayout = lazy(() => import('../components/layout/MainLayout'));
-// const About = lazy(() => import('../pages/About'));
-// const Home = lazy(() => import('../pages/Home'));
-// const AdminDashboard = lazy(()=> import('../pages/admin/AdminDashboard'));
-// const CreateStudent = lazy(()=> import('../pages/admin/CreateStudent'));
-
-// export const Routes : RouteObject[] = [
-//     {
-//       path: '/',
-//       element: <MainLayout />,
-//       children:[
-//         {
-//             index: true,
-//             element: <Home/>
-//         },
-//         {
-//             path: 'about',
-//             element: <About />,
-//           },
-//       ]
-//     },
-//     {
-//       path: '/admin',
-//       element: <MainLayout/>,
-//       children: [
-//         {
-//           index: true ,
-//           element: <AdminDashboard/>
-//         },
-//         {
-//           path: 'dashboard' ,
-//           element: <AdminDashboard/>
-//         }
-//         ,
-//         {
-//           path: 'create-student',
-//           element: <CreateStudent/>
-//         }
-//       ]
-//     }
-
-//   ];
 
 const AppRoutes = () => {
   return (
@@ -63,13 +15,7 @@ const AppRoutes = () => {
         <Route path="about" element={<About/>} />
       </Route>
 
-      <Route path="/admin" element={<MainLayout />}>
-        <Route index element={<AdminDashboard />} />
-        <Route path="dashboard" element={<AdminDashboard />} />
-        <Route path="create-admin" element={<CreateAdmin />} />
-        <Route path="create-student" element={<CreateStudent />} />
-        <Route path="create-faculty" element={<CreateFaculty />} />
-      </Route>
+      <AdminRoutes/>
 
       <Route path="/sign-in" element={<SignIn/>} />
       <Route path="/sign-up" element={<SignUp/>} />
