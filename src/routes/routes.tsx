bfb419aae1d2eaab6b5b6c79b951609154adcbe0
nazim1971 <md.nazimuddinaj@gmail.com>
2025-01-13 13:38:11 +0600

    Add admin routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,8 @@ import { RouteObject } from 'react-router';
 const MainLayout = lazy(() => import('../components/layout/MainLayout'));
 const About = lazy(() => import('../pages/About'));
 const Home = lazy(() => import('../pages/Home'));
+const AdminDashboard = lazy(()=> import('../pages/admin/AdminDashboard') );
+const CreateStudent = lazy(()=> import('../pages/admin/CreateStudent') )
 
 
 export const routes : RouteObject[] = [
@@ -21,6 +23,20 @@ export const routes : RouteObject[] = [
           },
       ]
     },
+    {
+      path: '/admin',
+      element: <MainLayout/>,
+      children: [
+        {
+          path: 'dashboard',
+          element: <AdminDashboard/>
+        },
+        {
+          path: 'create-student',
+          element: <CreateStudent/>
+        }
+      ]
+    }
    
      
   ];
