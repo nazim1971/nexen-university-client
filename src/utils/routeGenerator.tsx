913a73b9eb1f833529aeaa419baedc3b547a4c96
nazim1971 <md.nazimuddinaj@gmail.com>
2025-01-16 00:22:55 +0600

    add routeGenareetor

---
+++ src/utils/routeGenerator.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+import { TRoute } from "../types/route.types";
+
+
+export const generateRoutes = (paths: TRoute[]): ReactNode=>{
+    return paths.map((item)=>{
+        
+    })
+}
