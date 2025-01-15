84deda4e249d48a0fd761b7948e5e30eaa01e47e
nazim1971 <md.nazimuddinaj@gmail.com>
2025-01-16 00:27:24 +0600

    refactor route

--- src/utils/routeGenerator.tsx
+++ src/utils/routeGenerator.tsx
@@ -1,9 +1,22 @@
 import { ReactNode } from "react";
 import { TRoute } from "../types/route.types";
+import { Route } from "react-router";
 
-
-export const generateRoutes = (paths: TRoute[]): ReactNode=>{
-    return paths.map((item)=>{
-        
-    })
-}
+export const generateRoutes = (paths: TRoute[]): ReactNode => {
+    return paths.map((item) => {
+      if (item.children) {
+        return (
+          <Route key={item?.name} path={item?.path}>
+            {item?.element && <Route index element={item?.element} />}
+            {generateRoutes(item.children)}
+          </Route>
+        );
+      }
+  
+      return (
+        item?.path && (
+          <Route key={item.name} path={item.path} element={item.element} />
+        )
+      );
+    });
+  };
