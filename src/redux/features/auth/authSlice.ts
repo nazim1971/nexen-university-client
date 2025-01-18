9f19e522706861e3594f2990c6ba78edd511c1d1
nazim1971 <md.nazimuddinaj@gmail.com>
2025-01-18 11:46:12 +0600

    Add auth set

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -11,5 +11,21 @@ const initialState: TAUthState = {
 }
 
 const authSlice = createSlice({
-    name: 'auth'
+    name: 'auth',
+    initialState,
+    reducers: {
+        setUser: (state, action) =>{
+            const {user, token} = action.payload;
+            state.user = user;
+            state.token = token;
+        },
+        logout: (state) => {
+            state.user = null;
+            state.token = null;
+        }
+    }
 })
+
+export const {setUser, logout} = authSlice.actions;
+
+export default authSlice.reducer
